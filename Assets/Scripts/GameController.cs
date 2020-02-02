79956fa973214b435de5db447d1710101ab1150a
TrickyTriangles <47727993+TrickyTriangles@users.noreply.github.com>
2020-02-02 10:57:15 -0500

    EnemyDrops

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,11 +6,36 @@ public class GameController : MonoBehaviour
 {
     public List<GameObject> enemies;
 
+    // Player Resources
+    public int steel_count;
+    public int plastic_count;
+    public int clockwork_count;
+    public int sniper_count;
+    public int pulse_count;
+    public int shell_count;
+    public int damage_count;
+    public int slow_count;
+    public int acid_count;
+
+    // Ship parts
+    public bool has_engine;
+    public bool has_cable;
+    public bool has_computer;
+
     // Start is called before the first frame update
     void Start()
     {
         enemies = new List<GameObject>();
-        //enemies.AddRange(GameObject.FindGameObjectsWithTag("Enemy"));
+
+        steel_count = 0;
+        plastic_count = 0;
+        clockwork_count = 0;
+        sniper_count = 0;
+        pulse_count = 0;
+        shell_count = 0;
+        damage_count = 0;
+        slow_count = 0;
+        acid_count = 0;
     }
 
     // Update is called once per frame
@@ -38,6 +63,7 @@ public class GameController : MonoBehaviour
                 {
                     GameObject.Destroy(enemy);
                     ProcessEnemyDrop();
+                    ProcessShipDrop();
                 }
             }
         }
@@ -45,6 +71,81 @@ public class GameController : MonoBehaviour
 
     void ProcessEnemyDrop()
     {
-        Debug.Log("Ow!");
+        int option = (int)Random.Range(1f, 10f);
+        int quantity = (int)Random.Range(1f, 4f);
+
+        switch (option)
+        {
+            case 1:
+                steel_count += quantity;
+                Debug.Log("You got " + quantity.ToString() + " STEEL");
+                break;
+            case 2:
+                plastic_count += quantity;
+                Debug.Log("You got " + quantity.ToString() + " PLASTIC");
+                break;
+            case 3:
+                clockwork_count += quantity;
+                Debug.Log("You got " + quantity.ToString() + " CLOCKWORK");
+                break;
+            case 4:
+                sniper_count += quantity;
+                Debug.Log("You got " + quantity.ToString() + " SNIPER");
+                break;
+            case 5:
+                pulse_count += quantity;
+                Debug.Log("You got " + quantity.ToString() + " PULSE");
+                break;
+            case 6:
+                shell_count += quantity;
+                Debug.Log("You got " + quantity.ToString() + " SHELL");
+                break;
+            case 7:
+                damage_count += quantity;
+                Debug.Log("You got " + quantity.ToString() + " DAMAGE");
+                break;
+            case 8:
+                slow_count += quantity;
+                Debug.Log("You got " + quantity.ToString() + " SLOW");
+                break;
+            case 9:
+                acid_count += quantity;
+                Debug.Log("You got " + quantity.ToString() + " ACID");
+                break;
+            default:
+                break;
+        }
+    }
+
+    void ProcessShipDrop()
+    {
+        int option = (int)Random.Range(1f, 201f);
+
+        switch (option)
+        {
+            case 1:
+                if (!has_engine)
+                {
+                    has_engine = true;
+                    Debug.Log("You got the ENGINE!!");
+                }
+                break;
+            case 2:
+                if (!has_cable)
+                {
+                    has_cable = true;
+                    Debug.Log("You got the POWER CABLES!!");
+                }
+                break;
+            case 3:
+                if (!has_computer)
+                {
+                    has_computer = true;
+                    Debug.Log("You got the NAVIGATION COMPUTER!!");
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
